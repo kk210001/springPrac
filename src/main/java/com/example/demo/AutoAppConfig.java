1e3c26b3b42bb0c4d840c75c1078e7d91b4f55c5
kk210001 <kk210001@naver.com>
2022-05-18 19:32:15 +0900

    수동 빈 자동 빈 동시 등록 시 오류 발생 application.properties 파일에 spring.main.allow-bean-definition-overriding=false 등록으로 수동 빈 우선 등록

--- src/main/java/com/example/demo/AutoAppConfig.java
+++ src/main/java/com/example/demo/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import member.MemberRepository;
+import member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -15,5 +18,11 @@ import org.springframework.context.annotation.FilterType;
 
 
 public class AutoAppConfig {
+
+    //수동 빈 자동 빈 동시 등록 시 수동 빈 우선 등록이였다가 현재는 오류로 바뀜
+//    @Bean(name="memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 
 }
